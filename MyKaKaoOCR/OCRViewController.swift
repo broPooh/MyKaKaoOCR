cd19ba2f4a76a7b9ecc087ce858e8018cc67c907
broMac <yhs4705@naver.com>
2022-05-10 12:03:40 +0900

    ocr 통신 체크

--- MyKaKaoOCR/OCRViewController.swift
+++ MyKaKaoOCR/OCRViewController.swift
@@ -12,6 +12,9 @@ class OCRViewController: UIViewController {
     @IBOutlet weak var ocrImageView: UIImageView!
     @IBOutlet weak var sendButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    var resultString: [String] = []
+    //var resultString: String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +25,20 @@ class OCRViewController: UIViewController {
 
     @IBAction func sendButtonClicked(_ sender: UIButton) {
         
+        guard let image = ocrImageView.image else { return }
+        
+        OCRAPIManager.shared.fetchOcrData(image: image) { code, json in
+            //print("result", json["result"])
+            
+            for item in json["result"].arrayValue {
+                print(item["recognition_words"][0].stringValue)
+                self.resultString.append(item["recognition_words"][0].stringValue)
+                //self.resultString.append(" \(item["recognition_words"].stringValue.replacingOccurrences(of: "\"", with: ""))")
+            }
+            
+            self.resultLabel.text = self.resultString.joined(separator: " ")
+
+        }
     }
     
     
